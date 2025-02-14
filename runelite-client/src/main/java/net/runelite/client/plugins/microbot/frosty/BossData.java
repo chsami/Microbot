a9a2ceb3b49cb9c980799d3ae0cb895458cd4192
Travis Leonard <leonardstantont@gmail.com>
2025-02-14 14:19:14 -0600

    ArtioPrayerPlugin handles all prayer changing and restoring at Artio.
    Added myself to PluginDescriptor

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/frosty/BossData.java
@@ -0,0 +1,35 @@
+package net.runelite.client.plugins.microbot.frosty;
+
+import lombok.Getter;
+
+@Getter
+public enum BossData {
+    ARTIO(11992, 2350, 133, 10013, 10014, 10015, -1);
+
+    private final int npcId;
+    private final int rangeProjectile;
+    private final int mageProjectile;
+    private final int rangeAnimation;
+    private final int mageAnimation;
+    private final int meleeAnimation;
+    private final Integer betweenAnimation;
+
+    BossData(int npcId, int rangeProjectile, int mageProjectile, int rangeAnimation, int mageAnimation, int meleeAnimation, int betweenAnimation) {
+        this.npcId = npcId;
+        this.rangeProjectile = rangeProjectile;
+        this.mageProjectile = mageProjectile;
+        this.rangeAnimation = rangeAnimation;
+        this.mageAnimation = mageAnimation;
+        this.meleeAnimation = meleeAnimation;
+        this.betweenAnimation = betweenAnimation;
+    }
+
+    public static BossData fromNpcId(int id) {
+        for (BossData boss : values()) {
+            if (boss.npcId == id) {
+                return boss;
+            }
+        }
+        return null;
+    }
+}
