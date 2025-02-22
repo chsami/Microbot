19e130944a85fdf957308c16b5a7f6b85322563e
ElGuason <darwuinjose@gmail.com>
2025-02-22 17:06:19 -0400

    Add files via upload

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/SummerPies/SummerPiesConfig.java
@@ -0,0 +1,18 @@
+package net.runelite.client.plugins.microbot.SummerPies;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigInformation;
+import net.runelite.client.plugins.microbot.aiofighter.AIOFighterConfig;
+
+@ConfigGroup("SummerPies")
+@ConfigInformation("1. Asegúrese de tener los ingredientes en tu banco. <br /><br />" +
+        "Lista de ingredientes: <br />" +
+        "1000 Strawberry <br />" +
+        "1000 Watermelon <br />" +
+        "1000 Cooking apple <br />" +
+        "1000 Pie shell <br />")
+public interface SummerPiesConfig extends Config {
+}
+
+
