ee09fa52f268c212398fd3dab554e3bdf3f8fd8c
h5law <dev@h5law.com>
2025-03-28 17:10:43 +0000

    temp-fix: bug with loom and bank path findning in weaver-net

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/crafting/enums/Loom.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/crafting/enums/Loom.java
@@ -13,7 +13,7 @@ public enum Loom {
     HOSIDIUS("Hodidius", new WorldPoint(3033, 3288, 0), new WorldPoint(1676, 3617, 0)),
     SOUTH_FALADOR_FARM("South Falador Farm", new WorldPoint(3039, 3287, 0), new WorldPoint(1398, 2927, 0)),
     ALDARIN("Aldarin", new WorldPoint(3040, 3284, 0), new WorldPoint(1398, 2927, 0)),
-    MUSEUM_CAMP("Fossil Island", new WorldPoint(3033, 3285, 0), new WorldPoint(3739, 3804, 0));
+    MUSEUM_CAMP("Fossil Island", new WorldPoint(3730, 3822, 0), new WorldPoint(3739, 3804, 0));
 
     public final String label;
     public final WorldPoint loomWorldPoint;
