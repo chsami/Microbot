b54438cb9f7c3efe2d506cdd9d76231fb943ad3e
Victor Peralta <peralta.r.victor@gmail.com>
2025-05-01 23:57:00 -0700

    feat: add utility methods to ExamplePlugin

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/example/ExamplePlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/example/ExamplePlugin.java
@@ -2,12 +2,15 @@ package net.runelite.client.plugins.microbot.example;
 
 import com.google.inject.Provides;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.GameTick;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.plugins.microbot.MicrobotApi;
+import net.runelite.client.plugins.microbot.util.player.Rs2Player;
+import net.runelite.client.plugins.microbot.util.walker.Rs2Walker;
 import net.runelite.client.ui.overlay.OverlayManager;
 
 import javax.inject.Inject;
@@ -61,6 +64,38 @@ public class ExamplePlugin extends Plugin {
             ticks = 10;
         }
 
+    }
+
+    private boolean walkTo(int x, int y, int z){
+        return Rs2Walker.walkTo(x, y, z);
+    }
+
+    private void goToCity(String name){
+
+    }
+
+    private WorldPoint getPointFromRegionId(int regionId)
+    {
+        return WorldPoint.fromRegion(regionId,1 ,1,0);
+    }
+
+    private WorldPoint getClosestPointFromPlayer(WorldPoint[] points){
+        WorldPoint playerPoint = Rs2Player.getLocalPlayer().getWorldLocation();
+        WorldPoint closestPoint = null;
+        int closestDistance = Integer.MAX_VALUE;
+        for (WorldPoint point : points) {
+            int distance = playerPoint.distanceTo(point);
+            if (distance < closestDistance) {
+                closestDistance = distance;
+                closestPoint = point;
+            }
+        }
+        return closestPoint;
+    }
+
+    private boolean followPlayerByName(String name){
+        var player  = Rs2Player.getPlayer(name);
+        return Rs2Player.follow(player);
     }
 
 }
