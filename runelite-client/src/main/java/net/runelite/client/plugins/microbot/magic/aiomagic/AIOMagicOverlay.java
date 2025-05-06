c9e02b4be1261cd0c583ef490f2467df5d251118
Victor Peralta <peralta.r.victor@gmail.com>
2025-05-05 21:28:10 -0700

    Undo changes to other files

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/magic/aiomagic/AIOMagicOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/magic/aiomagic/AIOMagicOverlay.java
@@ -1,6 +1,7 @@
 package net.runelite.client.plugins.microbot.magic.aiomagic;
 
 import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.plugins.microbot.example.ExamplePlugin;
 import net.runelite.client.ui.overlay.OverlayPanel;
 import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.components.LineComponent;
