93b450e0e2cc4fd834f6f93653f31f871575281c
TheHaliax <The.Haliax@gmail.com>
2025-06-17 20:31:12 -0500

    Reapply "Rs2Bank API and InventorySetups locked slots"
    This reverts commit 8f86e1fc3c813c09e88e6ada76464c9759dde536.

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/inventorysetups/InventorySetupsAmmoHandler.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/inventorysetups/InventorySetupsAmmoHandler.java
@@ -169,7 +169,7 @@ public class InventorySetupsAmmoHandler
 				int runeAmount = client.getVarbitValue(RUNE_POUCH_AMOUNT_VARBITS.get(i));
 				String runeName = itemManager.getItemComposition(runeId).getName();
 				InventorySetupsStackCompareID stackCompareType = panel.isStackCompareForSlotAllowed(InventorySetupsSlotID.RUNE_POUCH, i) ? config.stackCompareType() : InventorySetupsStackCompareID.None;
-				runePouchData.add(new InventorySetupsItem(runeId, runeName, runeAmount, false, stackCompareType));
+				runePouchData.add(new InventorySetupsItem(runeId, runeName, runeAmount, false, stackCompareType, false, -1));
 			}
 		}
 
@@ -209,7 +209,7 @@ public class InventorySetupsAmmoHandler
 				InventorySetupsStackCompareID stackCompareType =
 						panel.isStackCompareForSlotAllowed(InventorySetupsSlotID.BOLT_POUCH, i)
 								? config.stackCompareType() : InventorySetupsStackCompareID.None;
-				boltPouchData.add(new InventorySetupsItem(boltItemId, boltName, boltAmount, false, stackCompareType));
+				boltPouchData.add(new InventorySetupsItem(boltItemId, boltName, boltAmount, false, stackCompareType, false, -1));
 			}
 		}
 
@@ -246,8 +246,7 @@ public class InventorySetupsAmmoHandler
 					panel.isStackCompareForSlotAllowed(InventorySetupsSlotID.QUIVER, 0)
 							? config.stackCompareType() : InventorySetupsStackCompareID.None;
 
-			final InventorySetupsItem quiverItem = new InventorySetupsItem(quiverAmmoId, ammoName,
-					quiverAmmoCount, false, stackCompareType);
+			final InventorySetupsItem quiverItem = new InventorySetupsItem(quiverAmmoId, ammoName, quiverAmmoCount, false, stackCompareType, false, -1);
 			quiverData.add(quiverItem);
 		}
 
--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/runecrafting/gotr2/data/Phase.java
+++
@@ -1,7 +0,0 @@
-package net.runelite.client.plugins.microbot.runecrafting.gotr2.data;
-
-public enum Phase {
-    NOT_IN_GAME,
-    PRE_PORTAL,
-    POST_PORTAL_LOOP
-}
