4996a621789b585cb4898f6bf4fa71c2d8bf1c1f
vi3tcn <60859658+vi3tcn@users.noreply.github.com>
2025-07-08 05:34:32 +0100

    Update HarpoonType.java
    Edited for ItemVariationMapping so should work with all variants or ornament kits and uncharged harpoons

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/tempoross/enums/HarpoonType.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/tempoross/enums/HarpoonType.java
@@ -2,40 +2,31 @@ package net.runelite.client.plugins.microbot.tempoross.enums;
 
 import net.runelite.api.AnimationID;
 import net.runelite.api.ItemID;
+import net.runelite.client.game.ItemVariationMapping;
 
 public enum HarpoonType
 {
-
-// HARPOON, BARBTAIL_HARPOON, DRAGON_HARPOON, INFERNAL_HARPOON, CRYSTAL_HARPOON
-
     HARPOON(ItemID.HARPOON, AnimationID.FISHING_HARPOON, "Harpoon"),
     BAREHAND(-1, AnimationID.FISHING_BAREHAND, "Bare-handed"),
     BARBTAIL_HARPOON(ItemID.BARBTAIL_HARPOON, AnimationID.FISHING_BARBTAIL_HARPOON, "Barb-tail harpoon"),
     DRAGON_HARPOON(ItemID.DRAGON_HARPOON, AnimationID.FISHING_DRAGON_HARPOON, "Dragon harpoon"),
-    DRAGON_HARPOON_OR(ItemID.DRAGON_HARPOON_OR, AnimationID.FISHING_TRAILBLAZER_HARPOON, "Dragon harpoon (or)"),
-    DRAGON_HARPOON_OR_RELOADED(ItemID.DRAGON_HARPOON_OR_30349, AnimationID.FISHING_TRAILBLAZER_HARPOON, "Dragon harpoon reloaded (or)"),                
     INFERNAL_HARPOON(ItemID.INFERNAL_HARPOON, AnimationID.FISHING_INFERNAL_HARPOON, "Infernal harpoon"),
-    INFERNAL_HARPOON_UNCHARGED(ItemID.INFERNAL_HARPOON_UNCHARGED, AnimationID.FISHING_INFERNAL_HARPOON, "Infernal harpoon (uncharged)"),
-    INFERNAL_HARPOON_OR(ItemID.INFERNAL_HARPOON_OR_30342, AnimationID.FISHING_TRAILBLAZER_HARPOON, "Infernal harpoon (or)"),
-    INFERNAL_HARPOON_OR_UNCHARGED(ItemID.INFERNAL_HARPOON_UNCHARGED_30343, AnimationID.FISHING_TRAILBLAZER_HARPOON, "Infernal harpoon uncharged (or)"),
-    CRYSTAL_HARPOON(ItemID.CRYSTAL_HARPOON, AnimationID.FISHING_CRYSTAL_HARPOON, "Crystal harpoon"),
-    CRYSTAL_HARPOON_INACTIVE(ItemID.CRYSTAL_HARPOON_INACTIVE, AnimationID.FISHING_CRYSTAL_HARPOON, "Crystal harpoon (inactive)");
+    CRYSTAL_HARPOON(ItemID.CRYSTAL_HARPOON, AnimationID.FISHING_CRYSTAL_HARPOON, "Crystal harpoon");
 
-
-    private final int id;
+    private final int canonicalItemId;
     private final int animationId;
     private final String name;
 
-    HarpoonType(int id, int animationId, String name)
+    HarpoonType(int itemId, int animationId, String name)
     {
-        this.id = id;
+        this.canonicalItemId = ItemVariationMapping.map(itemId); // canonicalize here
         this.animationId = animationId;
         this.name = name;
     }
 
-    public int getId()
+    public int getCanonicalItemId()
     {
-        return id;
+        return canonicalItemId;
     }
 
     public int getAnimationId()
@@ -48,5 +39,18 @@ public enum HarpoonType
         return name;
     }
 
+    public static HarpoonType fromItemId(int itemId)
+    {
+        int canonical = ItemVariationMapping.map(itemId);
 
+        for (HarpoonType type : values())
+        {
+            if (type.canonicalItemId == canonical)
+            {
+                return type;
+            }
+        }
+
+        return null;
+    }
 }
