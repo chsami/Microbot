d9784d79caaac94820ca296cf8da350a056b6ee7
by-grace-alone <caleb@bub3l.com>
2025-08-14 08:01:09 -0500

    New plugin: Auto Fishing (catch any fish + Barb fishing + Karambwan fishing + Anglerfish, harpoon specs, bank clues and caskets)

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/bga/autofishing/enums/AutoFishingState.java
@@ -0,0 +1,15 @@
+package net.runelite.client.plugins.microbot.bga.autofishing.enums;
+
+public enum AutoFishingState {
+
+    INITIALIZING,
+    CHECKING_GEAR,
+    GETTING_GEAR,
+    TRAVELING,
+    FISHING,
+    MANAGING_SPEC,
+    INVENTORY_FULL,
+    DEPOSITING,
+    RETURNING,
+    ERROR_RECOVERY
+}
